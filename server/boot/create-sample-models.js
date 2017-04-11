da90b8eadf93a96d09ad4484ab0ce5bfec77c5a0
Yordan Georgiev <iordan340@gmail.com>
2017-04-11 15:52:45 +0300

    Demo api

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,42 +1,16 @@
 module.exports = function(app) {
-  app.dataSources.postgresDS.automigrate('CoffeeShop', function(err) {
+  app.dataSources.postgresDS.automigrate('DemoObject', function(err) {
     if(err) throw err;
 
-    app.models.CoffeeShop.create([{
-      name: 'Bel Cafe',
-      city: 'Vancouver',
-      capacity: 111
-    }, {
-      name: 'Three Bees Coffee House',
-      city: 'San Mateo',
-      capacity: 222
-    }, {
-      name: 'Caffe Artigiano',
-      city: 'Vancouver',
-      capacity: 333
-    },], function(err, coffeeShops) {
+    app.models.DemoObject.create([{
+      stringAttr: 'Hardcoded string#1',
+      numberAttr: 1,
+      booleanAttr: true
+    }], function(err, demoObjects) {
       if(err) throw err;
 
-      console.log('Models created: \n', coffeeShops);
+      console.log('Models created: \n', demoObjects);
     });
   });
 
-  app.dataSources.postgresDS.automigrate('Product', function(err) {
-    if(err) throw err;
-
-    app.models.Product.create([{
-      title: 'Bel Cafe',
-      quantity: 1
-    }, {
-      title: 'Three Bees Coffee House',
-      quantity: 2
-    }, {
-      title: 'Caffe Artigiano',
-      quantity: 3
-    },], function(err, products) {
-      if(err) throw err;
-
-      console.log('Models created: \n', products);
-    });
-  });
 };
